f40ba4c155bea6e116465c343fb0a326637a0d49
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-09 16:20:31 +0000

    Edited return_to_mother_app

--- client_code/return_to_mother_app.py
+++ client_code/return_to_mother_app.py
@@ -9,8 +9,8 @@ mais seulement d'un module !
 """ This part will have to be adapted to any APP callind SIGN UP app """
 """ ************************************************************************"""  
 def calling_mother_app(x=2):
-    import Fitness_d
+    import AMS_Data
     #from Fitness_d.home_form import home_form
     #open_form('Fitness_d.home_form',x)    #x=3 si login normale
-    from Fitness_d import Module1
+    from AMS_Data import Module1
     Module1.starting_app(x)  # ds main app 2+1 donc pas de retour ds Sign In
