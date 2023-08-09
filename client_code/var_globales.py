68b40e20b4a908bb0f7f885fbbb975c65777c126
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-08-09 20:46:39 +0000

    Edited settings and var_globales

--- client_code/var_globales.py
+++ client_code/var_globales.py
@@ -6,9 +6,9 @@ from anvil.google.drive import app_files
 import anvil.server
 
 # Variables globales que j'appelle par 'var_globales.code_app'
-code_app1 = "https://2erggvosxp234ktt.anvil.app/EK44RJHJVMRZ7R52DY6NEURF"   # App "AMS Data"  
+code_app1 = "https://sxgqveyu3c2nj5kr.anvil.app/BBDLB54NWHHYKEE3YYGJGU4G"   # App "AMS Data"  
 code_app2 = "https://2erggvosxp234ktt.anvil.app/EK44RJHJVMRZ7R52DY6NEURF" # App "sign-up_for_AMS_Data"
-nom_app_pour_mail = "AMS Data"                                          
-mon_mail = "jmarc@formation-et-services.fr"
+nom_app_pour_mail = "AMSport Data"                                          
+mon_mail = "jmarc@jmm-formation-et-services.fr"
 mon_logo = "logo_fs_small.png"
 timedelay_url_in_min = 10 #10 minutes delay, after URL (mail-confirm or pw-reset) over 
