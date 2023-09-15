71323ac1d8b53f93824ad86ee74699c163cf0511
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2023-09-15 20:15:52 +0000

    Edited LoginDialog_V2

--- client_code/LoginDialog_V2/__init__.py
+++ client_code/LoginDialog_V2/__init__.py
@@ -6,6 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from .. import login_flow
+from .. import return_to_mother_app
 
 class LoginDialog_V2(LoginDialog_V2Template):
     def __init__(self, **properties):
@@ -15,35 +16,65 @@ class LoginDialog_V2(LoginDialog_V2Template):
         # Any code you write here will run when the form opens.
         self.email_box.focus()
 
-    def confirm_lnk_click (self, **event_args):
-        """Close any alert we might be in with 'confirm_email' value."""
-        self.raise_event('x-close-alert', value='confirm_email')
+    
 
-    def reset_pw_link_click (self, **event_args):
-        """Close any alert we might be in with 'reset_password' value."""
-        self.raise_event('x-close-alert', value='reset_password')
+    def email_box_lost_focus(self, **event_args):
+        """This method is called when the TextBox loses focus"""
+        pass
+        
 
-    def focus_password(self, **kws):
-        """Focus on the password box."""
-        self.password_box.focus()
-
-    def close_alertmel(self, **kws):
-        """Close any alert we might be in with 'login' value."""
-        self.raise_event('x-close-alert', value='login')
-
-    def form_show(self, **event_args):
-        """This method is called when the column panel is shown on the screen"""
-        self.email_box.focus()
-
-    def button_validation_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        alert("validation")
+    def password_box_lost_focus(self, **event_args):
+        """This method is called when the TextBox loses focus"""
+        pass
+        
 
     def button_retour_click(self, **event_args):
         """This method is called when the button is clicked"""
-        from AMS_Data.Main import Main
+        return_to_mother_app.calling_mother_app()
+
+    def button_validation_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        # --------------------------------Tests sur mail
+        # mail vide ?
+        if self.email_box.text == "":
+            alert("Entrez votre mail svp !")
+            return
+        # mail en minuscule    et strip
+        mel = self.email_box.text
+        mel = mel.lower()
+        mel = mel.strip()
+        self.email_box.text = mel
         
-        Main.content_panel.clear()
-        return
+        # @ ds mail ?
+        if not "@" in self.email_box.text:
+            alert("Entrez un mail valide !")
+            self.email_box.focus()
+            return
+        # --------------------------------Tests sur mot de passe   
+        if self.password_box.text == "":
+            alert("Entrez le mot de passe svp !")
+            self.password_box.focus()
+            return   
+        # ------------------------------------------------------------   VALIDATION 
+        try:
+            user=anvil.users.login_with_email(self.email_box.text, self.password_box.text, remember=True)
+            user=anvil.server.call("force_log",user)
+            return_to_mother_app.calling_mother_app(2)
+        except anvil.users.EmailNotConfirmed:
+            alert("Votre mail n'est pas encore confirmé! Vérifiez le mail que nous vs avions envoyé !")
+            if anvil.server.call('_send_email_confirm_link', self.email_box.text):
+                alert(f"Un nouvel email de confirmation vous a été envoyé à {email_box.text}.")
+                return_to_mother_app.calling_mother_app()
+        except anvil.users.AuthenticationFailed as e:
+            alert(f"Email ou mot de passe erroné, Ré-entrez les !")
+            return
+
+    def password_box_pressed_enter(self, **event_args):
+        """This method is called when the user presses Enter in this text box"""
+        self.button_validation_click()
+
+
+        
+        
         
 
