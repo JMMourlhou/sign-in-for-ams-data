f596aa7ab0b2e203f8fcca3329988c298386184b
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-03-10 21:39:35 +0000

    Edited settings, ServerFunctions and api_sign_in_search

--- server_code/ServerFunctions.py
+++ server_code/ServerFunctions.py
@@ -102,7 +102,7 @@ def do_signup(email, name, password, num_stage):
     api = mk_api_key()
     date_heure = french_zone.time_french_zone()
   
-    user = app_tables.users.add_row(email=email.lower(),role="S", enabled=True, nom=name, password_hash=pwhash, api_key=api, signed_up=date_heure, temp=int(num_stage), histo={})
+    user = app_tables.users.add_row(email=email.lower(),role="S", enabled=True, nom=name, password_hash=pwhash, api_key=api, signed_up=date_heure, temp=int(num_stage))
     print("création user", user['email'])
     err = None # pas d'erreur
   else:  # erreur 
