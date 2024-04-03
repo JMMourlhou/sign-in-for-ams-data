b391f765b519e5c7df31f445caf7553fe2987626
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-04-03 09:29:04 +0000

    Edited var_globales

--- client_code/var_globales.py
+++ client_code/var_globales.py
@@ -11,4 +11,4 @@ code_app2 = "https://2erggvosxp234ktt.anvil.app/EK44RJHJVMRZ7R52DY6NEURF"   # Ap
 nom_app_pour_mail = "AMSport Data"                                         
 mon_mail = "jmarc@jmm-formation-et-services.fr"
 mon_logo = "logo_fs_small.png"
-timedelay_url_in_min = 60 * 24 * 7 #  60 min x 24 hours x 7 days     delay, after URL (mail-confirm or pw-reset) over 
+timedelay_url_in_min = 60 * 24 * 1000 #  60 min x 24 hours x 7 days     delay, after URL (mail-confirm or pw-reset) over 
