0365b6d5bcc8497d9e0e9f3a8f1545f32228cd12
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-04-12 20:10:49 +0000

    Edited ServerFunctions

--- server_code/ServerFunctions.py
+++ server_code/ServerFunctions.py
@@ -44,8 +44,8 @@ def _send_password_reset(email):
   user = app_tables.users.get(email=email)
   t=recup_time() # t will be text form (module at the end of this server code module)
   if user is not None:
-    logo_address = var_code_app2+"/_/theme/"+var_globales.mon_logo
-    anvil.email.send(to=user['email'], subject=var_globales.nom_app_pour_mail + "Reinitilisez votre mot de passe",
+    logo_address = code_app2+"/_/theme/"+mon_logo
+    anvil.email.send(to=user['email'], subject=nom_app_pour_mail + "Reinitilisez votre mot de passe",
 html=f"""
 <p><img src = {logo_address} width="200" height="200"> </p> 
 <b>Mme/Mr {user["nom"]},</b><br>
@@ -55,11 +55,11 @@ Avez-vous bien demandé une modification du mot de passe de votre compte ? Si ce
 Si vous désirez poursuivre et ré-initialiser votre mot de passe, <b>clickez le lien ci-dessous:</b>
 <br>
 
-{var_globales.code_app1}/#?a=pwreset&email={url_encode(user['email'])}&api={url_encode(user['api_key'])}&t={t} <br>
+{code_app1}/#?a=pwreset&email={url_encode(user['email'])}&api={url_encode(user['api_key'])}&t={t} <br>
 <br><br>
 <b><i>         Jean-Marc</b></i>,<br>
 <b>JMM Formation & Services</b> <br>
-{var_globales.mon_mail} <br>
+{mon_mail} <br>
 """)
       
     return True
@@ -70,22 +70,22 @@ Si vous désirez poursuivre et ré-initialiser votre mot de passe, <b>clickez le
 @anvil.server.callable
 def _send_email_confirm_link(email):
   user = app_tables.users.get(email=email)
-  logo_address = var_globales.code_app2+"/_/theme/"+var_globales.mon_logo
+  logo_address = code_app2+"/_/theme/"+mon_logo
   t=recup_time() # t will be text form (module at the end of this server code module)
   if user is not None and not user['confirmed_email']:  # User table, Column confirmed_email not checked/True
-        anvil.email.send(to=user['email'], subject=var_globales.nom_app_pour_mail + "Confirmation de votre adresse email",
+        anvil.email.send(to=user['email'], subject=nom_app_pour_mail + "Confirmation de votre adresse email",
 html=f"""
 <p><img src = {logo_address} width="200" height="200"> </p> 
 <b>Mme/Mr {user["nom"]},</b><br>
 <br>
-Merci de votre enregistrement sur {var_globales.nom_app_pour_mail} !<br>
+Merci de votre enregistrement sur {nom_app_pour_mail} !<br>
 Afin de confirmer votre adresse mail, <b>clickez le lien ci-dessous:</b><br>
 <br>
-{var_globales.code_app1}/#?a=confirm&email={url_encode(user['email'])}&hpw={url_encode(user['password_hash'])}&t={t} <br>
+{code_app1}/#?a=confirm&email={url_encode(user['email'])}&hpw={url_encode(user['password_hash'])}&t={t} <br>
 <br><br>
 <b><i>         Jean-Marc</b></i>,<br>
 <b>JMM Formation & Services</b> <br>
-{var_globales.mon_mail} <br>
+{mon_mail} <br>
 """)
   return True
 
