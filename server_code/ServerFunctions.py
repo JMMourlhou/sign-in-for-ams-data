272984e256cbd3c42ef9c1dde915a4eec50b323a
jmmourlhou@gmail.com <jmmourlhou@gmail.com>
2024-10-18 21:40:55 +0000

    Edited settings, ServerFunctions and api_sign_in_search

--- server_code/ServerFunctions.py
+++ server_code/ServerFunctions.py
@@ -62,7 +62,7 @@ Si vous désirez poursuivre et ré-initialiser votre mot de passe, <b>clickez le
 {code_app1}/#?a=pwreset&email={url_encode(user['email'])}&api={url_encode(user['api_key'])}&t={t} <br>
 <br><br>
 <b><i>         Jean-Marc</b></i>,<br>
-<b>JMM Formation & Services</b> <br>
+<b>jmweb34.org</b> <br>
 {mon_mail} <br>
 """)
       
@@ -91,7 +91,7 @@ Afin de confirmer votre adresse mail, <b>clickez le lien ci-dessous:</b><br>
 {code_app1}/#?a=confirm&email={url_encode(user['email'])}&hpw={url_encode(user['password_hash'])}&t={t} <br>
 <br><br>
 <b><i>         Jean-Marc</b></i>,<br>
-<b>JMM Formation & Services</b> <br>
+<b>jmweb34.org</b> <br>
 {mon_mail} <br>
 """)
   return True
--- server_code/api_sign_in_search.py
+++ server_code/api_sign_in_search.py
@@ -4,9 +4,9 @@ import anvil.email
 import anvil.users
 import tables
 from tables import app_tables
-import anvil.tables as tables
+#import anvil.tables as tables
 import anvil.tables.query as q
-from anvil.tables import app_tables
+#from anvil.tables import app_tables
 import anvil.server
 import bcrypt # to test the API key crypted
 from . import french_zone
